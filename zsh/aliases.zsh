eb247cf089855dd8f84f732c44a173493c4451d9
polikis <contacto.mail99@gmail.com>
2014-09-06 21:18:05 +0200

    Configure PATH and Anaconda
    Added helper functions to add and remove anaconda directory from file,
    because the anaconda environment conflicts with virtualenvwrapper. In
    anaconda is disabled, an alias will be created to ipython by default

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -2,7 +2,20 @@ if [[ -e ~/sage/sage ]]; then
 	alias sage=~/sage/sage
 fi
 
-# If ipython doesn't exist, alias sagemath's internal version 
+# If ipython doesn't exist, alias to anaconda ipython 
 if ! type ipython > /dev/null; then
-	alias ipython='sage -ipython'
+	alias ipython='$HOME/anaconda/bin/ipython'
 fi
+
+# Force PATH to contain only unique elements
+typeset -U path
+
+conda_start(){
+	# Add anaconda directory to PATH
+	path=("$HOME/anaconda/bin" "$path[@]")
+}
+
+conda_stop(){
+	# Remove anaconda direcotry from PATH
+	path=("${(@)path:#$HOME/anaconda/bin}")
+}
--- zsh/env.zsh
+++ zsh/env.zsh
@@ -1,5 +1,6 @@
 ### set PATH
-
+PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games"
+PATH="$PATH:$HOME/.rvm/bin" # Add RVM to PATH for scripting
 export PATH
 
 # If avalible, load nvm (Node Version Manager)
@@ -8,6 +9,8 @@ export PATH
 ### Python
 
 # Virtualenv Python
+export VIRTUALENVWRAPPER_PYTHON="/usr/bin/python" # Use default python
+export WORKON_HOME=$HOME/envs
 
 # pip should only run if there is a virtualenv currently activated
 # export PIP_REQUIRE_VIRTUALENV=true
@@ -16,4 +19,3 @@ export PATH
 export PIP_DOWNLOAD_CACHE=$HOME/.pip/cache
 
 source /usr/local/bin/virtualenvwrapper.sh
-
